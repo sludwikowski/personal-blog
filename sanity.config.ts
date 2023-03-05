57f447c4f31db5c4ecbaf123d41ba09d0e95bc8d
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-03-05 20:19:46 +0100

    `structure` created for roadToReact-studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,7 @@ import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 import { myTheme } from './theme'
+import { getDefaultDocumentNode } from './structure'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
@@ -11,11 +12,16 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
 
 export default defineConfig({
   basePath: '/studio',
-  name: 'My_personal-Blog',
-  title: 'My_personal-Blog',
+  name: 'roadToReact',
+  title: 'roadToReact',
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      defaultDocumentNode: getDefaultDocumentNode,
+    }),
+    visionTool(),
+  ],
   schema: {
     types: schemaTypes,
   },
