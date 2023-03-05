e7814e3ef68b0e6f8ef98e46302e452bdd53ee04
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-03-05 16:03:42 +0100

    change `Head` properties

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="xs:flex-row mt-4 flex w-full flex-col items-center justify-between gap-2 border-b border-gray-600 px-2 pb-5 sm:px-4 lg:flex-row">
       <Link href="/" className="flex space-x-2">
-        <Image alt="header text" src="/logo192.png" className="h-9 w-9 sm:h-10 sm:w-10" width={24} height={24} />
+        <Image alt="header text" src="/road.png" className="h-9 w-9 sm:h-8 sm:w-8" width={24} height={24} />
         <span className={'relative'}>
           <SquigglyLines />
           <h1 className="ml-2 text-xl font-bold tracking-tight sm:text-3xl">roadToReact</h1>
