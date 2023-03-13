8e254294c5e3d51b1fe79451691c2c544f0e5b8a
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-03-13 13:43:55 +0100

    delete `utils` folder

--- types/block.ts
+++ types/block.ts
@@ -9,35 +9,12 @@ export const baseTypedObjectZ = z
     _type: z.string(),
     _key: z.string().optional(),
   })
-  // Not happy we have to pass through, but this is the only way to
-  // satisfy TypedObject for `value` in @react/portable-text and the rest of the data through
   .passthrough()
 
-// This helper function will generate a zod parser for a given type
 export const typedObjectZ = schemaForType<TypedObject>()(baseTypedObjectZ)
 
 export const typedObjectBlockZ = baseTypedObjectZ.extend({
   style: z.string().optional(),
-  // children: z
-  //   .array(
-  //     z.object({
-  //       _type: z.string(),
-  //       _key: z.string(),
-  //       marks: z.array(z.string()).optional(),
-  //       text: z.string(),
-  //     })
-  //   )
-  //   .optional(),
-  // markDefs: z
-  //   .array(
-  //     z
-  //       .object({
-  //         _type: z.string(),
-  //         _key: z.string(),
-  //       })
-  //       .optional()
-  //   )
-  //   .optional(),
 })
 
 export type TypedObjectBlock = z.infer<typeof typedObjectBlockZ>
--- utils/scrollableId.ts
+++
@@ -1,4 +0,0 @@
-// If we're using hashes to scroll to it can be problematic if the id starts with a number
-export function scrollableKey(key: string) {
-  return `s-${key}`
-}
